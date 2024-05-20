16e7a9d38ed0cd0e2016d9e2dc9555f6cc23aa8a
MathGeniusJodie <mail@jodie.website>
2024-05-20 09:23:44 -0400

    added ascii art

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,56 @@ const TUX : [&str; 7] = [
     "\x1b[33m   /`\\     /`\\ \x1b[37m  ",
     "\x1b[33m   \\__)\x1b[37m===\x1b[33m(__/\x1b[37m   "
 ];
+
+const ALPINE : [&str; 7] = [
+    "\x1b[31m   .8BBBBBBBBB.   ",
+    "\x1b[33m  .888888888888.  ",
+    "\x1b[32m .8888* *88*8888. ",
+    "\x1b[36m 888* ,8, `. *888 ",
+    "\x1b[36m `* ,88888, `, *` ",
+    "\x1b[34m  `888888888888`  ",
+    "\x1b[35m   `8BBBBBBBBB`   "
+];
+
+const ARCH : [&str; 7] = [
+    "\x1b[31m        A         ",
+    "\x1b[31m       a8s        ",
+    "\x1b[33m      ao88s       ",
+    "\x1b[32m     a88888s      ",
+    "\x1b[36m    a88Y*Y88s     ",
+    "\x1b[34m   a88H   B8bs    ",
+    "\x1b[35m  /*`       `*\\   "
+];
+
+const DEBIAN : [&str; 7] = [
+    "\x1b[31m    ,gA88bq.   ",
+    "\x1b[33m   dP      `9. ",
+    "\x1b[32m  d7  ,*`.  )8 ",
+    "\x1b[36m  9:  A    ,Q* ",
+    "\x1b[34m  *1  `^vsv\"   ",
+    "\x1b[35m   *b          ",
+    "\x1b[35m     \"~.       "
+];
+
+const MINT : [&str; 7] = [
+    "\x1b[32m BBBBBBBBBBBs~.  ",
+    "\x1b[32m BB8  88**88**8, ",
+    "\x1b[32m   H  H  a  a  l ",
+    "\x1b[32m   H  H  H  H  H ",
+    "\x1b[32m   V, *88888* .H ",
+    "\x1b[32m   `8,_______.=H ",
+    "\x1b[32m     *YBBBBBBBBH ",
+];
+const UBUNTU : [&str; 7] = [
+    "\x1b[31m          \x1b[35m($)",
+    "\x1b[31m    \x1b[33m .\x1b[31m s88~..",
+    "\x1b[31m    \x1b[33m.8*  \x1b[31m `*9.",
+    "\x1b[31m \x1b[31m($) \x1b[33m8",
+    "\x1b[31m    \x1b[33m`6-  \x1b[35m _-8`",
+    "\x1b[31m     \x1b[33m`\x1b[35m ^88*``",
+    "\x1b[31m          \x1b[33m($) "
+];
+
 fn main() {
     let user = std::env::var("LOGNAME").unwrap_or("Unknown".to_string());
     let host = nixinfo::hostname().unwrap_or("Unknown".to_string());
