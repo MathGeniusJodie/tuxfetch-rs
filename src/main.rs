89f8aab72190ac0b76ff9a8c866c4ec166479364
MathGeniusJodie <mail@jodie.website>
2024-05-20 09:04:09 -0400

    added package count

--- src/main.rs
+++ src/main.rs
@@ -8,27 +8,28 @@ const TUX : [&str; 7] = [
     "\x1b[33m   \\__)\x1b[37m===\x1b[33m(__/\x1b[37m   "
 ];
 fn main() {
-    let logname = std::env::var("LOGNAME").unwrap(); // user
-    let host = nixinfo::hostname().unwrap();
-    let distro = nixinfo::distro().unwrap();
-    let kernel = nixinfo::kernel().unwrap();
-    let uptime = nixinfo::uptime().unwrap();
-    // todo: packages
-    let shell = std::env::var("SHELL").unwrap();
-    let terminal = nixinfo::terminal().unwrap();
-    let cpu = nixinfo::cpu().unwrap();
-    let gpu = nixinfo::gpu().unwrap();
-    let memory = nixinfo::memory().unwrap();
+    let user = std::env::var("LOGNAME").unwrap_or("Unknown".to_string());
+    let host = nixinfo::hostname().unwrap_or("Unknown".to_string());
+    let distro = nixinfo::distro().unwrap_or("Unknown".to_string());
+    let kernel = nixinfo::kernel().unwrap_or("Unknown".to_string());
+    let uptime = nixinfo::uptime().unwrap_or("Unknown".to_string());
+    let packages = nixinfo::packages("apt").unwrap_or(nixinfo::packages("pacman").unwrap_or(nixinfo::packages("dnf").unwrap_or(nixinfo::packages("apk").unwrap_or("Unknown".to_string()))));
+    let shell = std::env::var("SHELL").unwrap_or("Unknown".to_string());
+    let terminal = nixinfo::terminal().unwrap_or("Unknown".to_string());
+    let cpu = nixinfo::cpu().unwrap_or("Unknown".to_string());
+    let gpu = nixinfo::gpu().unwrap_or("Unknown".to_string());
+    let memory = nixinfo::memory().unwrap_or("Unknown".to_string());
 
     //construct string with 1 tux line, then pad, then the info
-    let output = " ".repeat(17) + &logname + "@" + &host + "\n" +
+    let output = " ".repeat(17) + &user + "@" + &host + "\n" +
                  TUX[0] + "Distro: " + &distro + "\n" +
                  TUX[1] + "Kernel: " + &kernel + "\n" +
                  TUX[2] + "Uptime: " + &uptime + "\n" +
-                 TUX[3] + "Shell: " + &shell + "\n" +
-                 TUX[4] + "Terminal: " + &terminal + "\n" +
-                 TUX[5] + "CPU: " + &cpu + "\n" +
-                 TUX[6] + "GPU: " + &gpu + "\n" +
+                 TUX[3] + "Packages: " + &packages + "\n" +
+                 TUX[4] + "Shell: " + &shell + "\n" +
+                 TUX[5] + "Terminal: " + &terminal + "\n" +
+                 TUX[6] + "CPU: " + &cpu + "\n" +
+                 &" ".repeat(17) + "GPU: " + &gpu + "\n" +
                  &" ".repeat(17) + "Memory: " + &memory + "\n";
     print!("{}", output);
 }
